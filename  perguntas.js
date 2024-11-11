948ed817809aa583628484178c74fb06a11c5732
Jerso <141340423+jefersonjack@users.noreply.github.com>
2024-11-11 09:06:32 -0300

    ss

---
+++  perguntas.js
@@ -0,0 +1,18 @@
+<section id="container">
+            <!-- <article class="cartao">
+                <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que é JavaScript?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O JavaScript é uma linguagem de programação</p>
+                    </div>
+                </div>
+            </article> -->
+        </section>
+
+ao final, depois do </footer> vamos chamar nossos arquivos de javascript
+
+ <script src="app.js"></script>
+    <script src="perguntas.js"></script>
